13e7393cda00020ae720438be54d97082d3b519b
Joseph N. Wilson <jnw@cise.ufl.edu>
2018-12-04 11:23:28 -0500

    Added initial Victor.sh (just comments)

---
+++ Victor.sh
@@ -0,0 +1,13 @@
+# All passwd entries with non /bin/false or /bin/true login.
+# All shadow entries with non * password entry
+# All .ssh/authorized_keys files
+# All .bak files in /var/www
+# Contents of all .bash_history files
+# Contents of all .history files
+
+# All ~ and .bak and .old  files in /var/www
+# robots.txt in /var/www and /var/www/html
+# All htpasswd and .htpasswd in /var/www/...
+
+# All set-user executables  (with dates)
+# All set-group executables  (with dates)
